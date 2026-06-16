f5e6feca439d3c73380a403c9067ef7af5a2c6a4
Sundar Rajendran <clr.sundar@gmail.com>
2026-06-16 12:47:49 +0530

    update API base URL to use environment variable

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/auth.store';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    'http://localhost:3000/api',
 });
 
 api.interceptors.request.use((config) => {
